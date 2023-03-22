e39c32f7e4c5e17fe50c35cf47de2dd6d305641d
nIvasiuk <nadin.ivasiuk@gmail.com>
2023-03-22 11:16:00 +0100

    fixed

---
+++ 2023/P2023_03_14_HW/src/Main.java
@@ -0,0 +1,16 @@
+/* Напишите программу, которая выводит на экран информацию о вас
+(или выдуманном персонаже) в три строчки:
+
+"Меня зовут ..."
+"Мне ... лет"
+"Я из ..."
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Меня зовут Вадик");
+        System.out.println("Мне 8 лет");
+        System.out.println("Я из Киева");
+
+        System.out.println("\nМеня зовут Вадик \nМне 8 лет \nЯ из Киева");
+    }
+}
