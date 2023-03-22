141b5dddbb57b9fb7812241ce8e89893f61fea99
nIvasiuk <nadin.ivasiuk@gmail.com>
2023-03-22 09:28:47 +0100

    20.03.2023 Lesson 05. HW Task 2.

---
+++ 2023/P2023_03_20_Les_05_HW/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println('\u00C4');
+        System.out.println('\u002B');
+        System.out.println('\u005B');
+        System.out.println('\u00C6');
+        System.out.println('\u00A5');
+    }
+}
