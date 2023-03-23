c0b6585baea996c878ac39ab14be3039c7ec559c
nIvasiuk <nadin.ivasiuk@gmail.com>
2023-03-23 21:31:13 +0100

    22/03/2023 Les 07. HW task 2

--- 2023/P2023_03_22_Les_07_HW/src/Main.java
+++ 2023/P2023_03_22_Les_07_HW/src/Main.java
@@ -14,11 +14,31 @@ public class Main {
         System.out.println("Введите температуру: ");
         double temperature = scanner.nextDouble();
 
-        if ( temperature > 39){
+        if (temperature > 39) {
             System.out.println("Нужно срочно обратиться к врачу");
         } else {
             System.out.println("Лечитесь дома");
         }
 
+        /*
+        Задание 2
+
+Пользователь вводит слово, если слово не начинается на букву ‘Q’,
+выведите в консоль фразу: «А я уже начал бояться”,
+в противном случае - «Не повезло».
+Сохраните результат проверки условия в переменную типа boolean
+и выведите ее в консоль.
+         */
+        System.out.println("Введите слово: ");
+        String word = scanner.next();
+        char firstLetter = word.charAt(0);
+        boolean dontQ = firstLetter != 'Q';
+        if (firstLetter != 'Q') {
+            System.out.println("А я уже начал бояться");
+            System.out.println(dontQ);
+        } else {
+            System.out.println("Не повезло");
+            System.out.println(dontQ);
+        }
     }
 }
