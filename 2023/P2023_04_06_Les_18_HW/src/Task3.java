1b74def6c41aaad08c6f770f75aa4a82dee84504
nIvasiuk <nadin.ivasiuk@gmail.com>
2023-04-24 22:32:05 +0200

    04/06/2023 Les 18 HW task 3+4

---
+++ 2023/P2023_04_06_Les_18_HW/src/Task3.java
@@ -0,0 +1,40 @@
+public class Task3 {
+    public static void main(String[] args) {
+
+        System.out.println(getSumOfArraysNumbers(new int[]{1, 2, 3}));
+        System.out.println();
+        System.out.println(getSumOfNumberDivByThreeAndFive(new int[] {15,9,1,30,5}));
+    }
+
+    /*
+    3
+Напишите метод, который принимает массив целых чисел и возвращает сумму всех элементов
+{5,9,1} -> 15
+     */
+    public static int getSumOfArraysNumbers(int[] array) {
+        int res = 0;
+        for (int i = 0; i < array.length; i++) {
+            res = res + array[i];
+        }
+        return res;
+    }
+
+    /*
+    4
+Напишите метод , который считает сумму всех элементов из массива целых чисел, которые делятся и на 3 и на 5
+{15,9,1,30,5} -> 45 т.е. 15+30
+     */
+
+    public static int getSumOfNumberDivByThreeAndFive(int [] array){
+        int res = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] % 3 == 0) && (array[i] % 5 == 0)) {
+                res = res + array[i];
+            } else {
+                res = res + 0;
+            }
+        }
+        return res;
+    }
+
+}
