14666508bc0861f6d822b6539f5daf41f9d3ed7c
nIvasiuk <nadin.ivasiuk@gmail.com>
2023-04-27 11:37:45 +0200

    04/11/2023 Les 19 HW task 2

--- 2023/P2023_04_11_Les_19_HW/src/Main.java
+++ 2023/P2023_04_11_Les_19_HW/src/Main.java
@@ -1,35 +1,56 @@
 public class Main {
     public static void main(String[] args) {
 
-        int [] array = {1, 8, 4};
+        int[] array = {1, 8, 0, 80, 145};
         System.out.println("-------- Task 1 -------");
         System.out.println(getMaxIndex(array));
+
+        System.out.println("-------- Task  -------");
+        System.out.println(getMinIndex(array));
     }
+
     /*
 1
 Написать метод, который возвращает ИНДЕКС максимального элемента в массиве
 */
-    public static int getMaxIndex(int [] array){
+    public static int getMaxIndex(int[] array) {
         int temp = 0;
         int j = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (temp < array[i]){
+        for (int i = 0; i < array.length; i++) {     // находим максиамальное значение в массиве
+            if (temp < array[i]) {
                 temp = array[i];
             }
         }
-        for ( j = 0; j < array.length; j++) {
-            if (temp == array[j]){
+        for (j = 0; j < array.length; j++) {       // находим индекс максимального значения в массиве
+            if (temp == array[j]) {
                 return j;
             }
         }
-        return j;
+        return -1;     // тут не уверенна, что именно надо возвращать.
     }
+
     /*
 
 2
-Написать метод, который возвращает ИНДЕКС минимального элеиента в массиве
+Написать метод, который возвращает ИНДЕКС минимального элемента в массиве
 */
+    public static int getMinIndex(int[] array) {
+        int i = 0;
+        int temp = array[i];
+        int j = 0;
 
+        for (i = 0; i < array.length; i++) {     // находим минимальное значение в массиве
+            if (temp > array[i]) {
+                temp = array[i];
+            }
+        }
+        for (j = 0; j < array.length; j++) {       // находим индекс минимального значения в массиве
+            if (temp == array[j]) {
+                return j;
+            }
+        }
+        return -1;     // тут не уверенна, что именно надо возвращать.
+    }
 
 
     /*
