5e954fbefee03948b611a2292b47a970c9704655
nIvasiuk <nadin.ivasiuk@gmail.com>
2023-05-15 23:14:58 +0200

    19/04/2023 Les 25 task Pirate

---
+++ 2023/P2023_04_19_Les_25_Tasks/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+
+       /*
+       Создайте класс пират Pirate со следующими полями:
+            name;
+            nickName;
+            boolean isDrunk;
+         Создать конструктор и toString
+         Создать в классе Pirate метод say
+         метод должен выводить в консоль
+         "It is a bad day" - если пират трезв
+         "Yohohoho, what a good life" - если пират пьян
+         Создайте несколько пиратов, вызовите метод say
+
+         *** сделайте метод drink
+         метод должен менять знaчение поля isDrunk на true
+        */
+
+        Pirate silver = new Pirate("John", "Silver", false);
+        System.out.println(silver);
+        silver.say(silver);
+        System.out.println("**** pirate takes Rom ****");
+        drink(silver);
+        silver.say(silver);
+
+        System.out.println();
+        Pirate sparrow = new Pirate("Jack", "Sparrow", true);
+        System.out.println(sparrow);
+        sparrow.say(sparrow);
+
+
+
+    }
+
+    public static void drink(Pirate pirate){
+        if (pirate.isDrunk == false){
+            pirate.isDrunk = true;
+        }
+    }
+
+}
---
+++ 2023/P2023_04_19_Les_25_Tasks/src/Pirate.java
@@ -0,0 +1,48 @@
+public class Pirate {
+     /*     name;
+            nickName;
+            boolean isDrunk;
+         Создать конструктор и toString
+
+         *** сделайте метод drink
+         метод должен менять знaчение поля isDrunk на true
+        */
+
+    String name;
+    String nickName;
+    boolean isDrunk;
+
+    Pirate(String name, String nickName, boolean isDrunk){
+      this.name = name;
+      this.nickName = nickName;
+      this.isDrunk = isDrunk;
+    }
+
+  @Override
+  public String toString() {
+        if (isDrunk == true){
+            return  name + " " +
+                    nickName +
+                    ". Hi is drank.";
+        }
+      return  name + " " +
+              nickName +
+              ". He is sober.";
+  }
+
+  /*
+  Создать в классе Pirate метод say
+         метод должен выводить в консоль
+         "It is a bad day" - если пират трезв
+         "Yohohoho, what a good life" - если пират пьян
+         Создайте несколько пиратов, вызовите метод say
+   */
+
+    public void say(Pirate pirate){
+        if (isDrunk == false){
+            System.out.println("It is a bad day");
+        } else {
+            System.out.println("Yohohoho, what a good life");
+        }
+    }
+}
